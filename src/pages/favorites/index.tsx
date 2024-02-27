3c2d47b0bd48ba246876ccc34acaa9cf6109f045
AlexGioffDev <alex.gioffre_dev@outlook.it>
2024-02-27 18:08:09 +0100

    refactoring for deploying

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -3,7 +3,7 @@ import { GlobalContext } from "../../context";
 import RecipeItem from "../../components/recipe-item";
 
 export default function Favorites() {
-  const { favoritesList } = useContext(GlobalContext);
+  const { favoritesList } = useContext(GlobalContext)!;
 
   return (
     <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
